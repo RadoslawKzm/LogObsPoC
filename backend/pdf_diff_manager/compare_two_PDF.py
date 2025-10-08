122b06473b6f835317edffd7625b4fae38075063
Jan Dyndor <dyndorjan@gmail.com>
2025-10-08 21:19:19 +0200

    Example of findinf diffs in PDFs

---
+++ backend/pdf_diff_manager/compare_two_PDF.py
@@ -0,0 +1,33 @@
+from text_from_pdf_extractor import export_text_from_pdf
+import difflib
+
+file_one_path = ".\sample_pdf_file\sample-local-pdf.pdf"
+file_two_path = ".\sample_pdf_file\SamplePDFPage2.pdf"
+
+
+
+def compare_texts_first_approach(*, first_file_path : str, second_file_path : str):
+    first_text = export_text_from_pdf(file_path=first_file_path)
+    second_text = export_text_from_pdf(file_path=second_file_path)
+
+    differ = difflib.Differ()
+    result = list(differ.compare(first_text, second_text))
+    print(''.join(result))
+
+
+def compare_texts_second_approach(*, first_file_path: str, second_file_path: str):
+    first_text = export_text_from_pdf(file_path=first_file_path)
+    second_text = export_text_from_pdf(file_path=second_file_path)
+
+    diff = difflib.unified_diff(
+        first_text.splitlines(keepends=True),
+        second_text.splitlines(keepends=True),
+        fromfile=file_one_path,
+        tofile=file_two_path,
+        lineterm=''
+    )
+
+    print('\n'.join(diff))
+
+compare_texts_first_approach(first_file_path=file_one_path, second_file_path=file_two_path)
+compare_texts_second_approach(first_file_path=file_one_path, second_file_path=file_two_path)
